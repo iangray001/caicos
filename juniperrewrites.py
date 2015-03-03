20b658cd8082759568ae46a07fafac1843a9614b
Ian Gray <iangray001@github.com>
2015-03-03 11:44:49 +0000

    Converted Python indentation to tabs

--- juniperrewrites.py
+++ juniperrewrites.py
@@ -7,84 +7,84 @@ RAM_ACCESS_NAME = 'juniper_ram_fetch_'
 
 
 def add_parent_links(ast):
-    """
-    Iterate over an AST annotating every node with a link to its parent node.
-    """
-    def recurse(node, parent):
-        node.parent = parent
-        for _, c in node.children():
-            recurse(c, node)
-    recurse(ast, None)
+	"""
+	Iterate over an AST annotating every node with a link to its parent node.
+	"""
+	def recurse(node, parent):
+		node.parent = parent
+		for _, c in node.children():
+			recurse(c, node)
+	recurse(ast, None)
 
 
 def replace_node(orignode, newnode):
-    """
-    Replace the node orignode with the node newnode
-    """
-    for (child_name, child) in orignode.parent.children():
-        if child == orignode:
-            if '[' in child_name:
-                arrayname = child_name[:child_name.find('[')]
-                arrayidx = child_name[child_name.find('[')+1:child_name.find(']')]
-                try:
-                    arrayidx = int(arrayidx)
-                except:
-                    print str(arrayidx) + " is not a valid array index"
-                
-                getattr(orignode.parent, arrayname)[arrayidx] = newnode
-            else:
-                setattr(orignode.parent, child_name, newnode)
+	"""
+	Replace the node orignode with the node newnode
+	"""
+	for (child_name, child) in orignode.parent.children():
+		if child == orignode:
+			if '[' in child_name:
+				arrayname = child_name[:child_name.find('[')]
+				arrayidx = child_name[child_name.find('[')+1:child_name.find(']')]
+				try:
+					arrayidx = int(arrayidx)
+				except:
+					print str(arrayidx) + " is not a valid array index"
+				
+				getattr(orignode.parent, arrayname)[arrayidx] = newnode
+			else:
+				setattr(orignode.parent, child_name, newnode)
 
-                
+				
 
 def rewrite_RAM_structure_dereferences(ast):
-    """
-    The replacement Jamaica headers use RAM_NAME as a global pointer to the external bus interface. 
-    Unfortunately, JamaicaBuilder assumes that the type of this pointer is a complex 
-    union-of-structs-of-arrays. This rewrites such accesses to use a set of memory access functions
-    declared in fpgaporting.h that allow a more HLS-friendly representation of RAM.
-    """
-    class Visitor(c_ast.NodeVisitor):
-        """
-        We are looking for structures like the following:
-          ArrayRef:
-            StructRef: .
-              ArrayRef: 
-                ID: __juniper_ram_master
-                <Anything else>
-              ID: f
-            Constant: int, 2
-        """ 
-        def visit_StructRef(self, node):
-            c = node.children()
-            if len(c) == 2:
-                if isinstance(c[0][1], c_ast.ArrayRef) and isinstance(c[1][1], c_ast.ID):
-                    refch = c[0][1].children()
-                    if len(refch) > 0 and isinstance(refch[0][1], c_ast.ID):
-                        if getattr(refch[0][1], 'name') == RAM_NAME:
-                            
-                            #Appropriate node found.
-                            structmember = c[1][1].name #What was dereferenced ('i', 's', 'c', 'r', etc.)
-                            print str(node.coord) + ":\n\tOriginal: " + utils.getlineoffile(node.coord.file, node.coord.line).strip()
-                            functionargs = c_generator.CGenerator().visit(refch[1][1])
-                            
-                            #Is there a higher-level ArrayRef we are interested in?
-                            if isinstance(node.parent, c_ast.ArrayRef):
-                                if len(node.parent.children()) >= 2 and isinstance(node.parent.children()[1][1], c_ast.Constant):
-                                    arrayrefoffset = c_generator.CGenerator().visit(node.parent.children()[1][1])
-                                    call = RAM_ACCESS_NAME + structmember + "(" + functionargs + ", " + str(arrayrefoffset) + ")"
-                                    print "\tReplacement: " + call
-                                    replacementnode = c_ast.ID("name")
-                                    setattr(replacementnode, 'name', call)
-                                    replace_node(node.parent, replacementnode)
-                                else:
-                                    print "Unsupported ArrayRef format detected"
-                            else:
-                                call = "juniper_ram_fetch_" + structmember + "(" + functionargs + ", 0)"
-                                print "\tReplacement: " + call
-                                replacementnode = c_ast.ID("name")
-                                setattr(replacementnode, 'name', call)
-                                replace_node(node, replacementnode)
-                        
-    v = Visitor()
-    v.visit(ast)
+	"""
+	The replacement Jamaica headers use RAM_NAME as a global pointer to the external bus interface. 
+	Unfortunately, JamaicaBuilder assumes that the type of this pointer is a complex 
+	union-of-structs-of-arrays. This rewrites such accesses to use a set of memory access functions
+	declared in fpgaporting.h that allow a more HLS-friendly representation of RAM.
+	"""
+	class Visitor(c_ast.NodeVisitor):
+		"""
+		We are looking for structures like the following:
+		  ArrayRef:
+			StructRef: .
+			  ArrayRef: 
+				ID: __juniper_ram_master
+				<Anything else>
+			  ID: f
+			Constant: int, 2
+		""" 
+		def visit_StructRef(self, node):
+			c = node.children()
+			if len(c) == 2:
+				if isinstance(c[0][1], c_ast.ArrayRef) and isinstance(c[1][1], c_ast.ID):
+					refch = c[0][1].children()
+					if len(refch) > 0 and isinstance(refch[0][1], c_ast.ID):
+						if getattr(refch[0][1], 'name') == RAM_NAME:
+							
+							#Appropriate node found.
+							structmember = c[1][1].name #What was dereferenced ('i', 's', 'c', 'r', etc.)
+							print str(node.coord) + ":\n\tOriginal: " + utils.getlineoffile(node.coord.file, node.coord.line).strip()
+							functionargs = c_generator.CGenerator().visit(refch[1][1])
+							
+							#Is there a higher-level ArrayRef we are interested in?
+							if isinstance(node.parent, c_ast.ArrayRef):
+								if len(node.parent.children()) >= 2 and isinstance(node.parent.children()[1][1], c_ast.Constant):
+									arrayrefoffset = c_generator.CGenerator().visit(node.parent.children()[1][1])
+									call = RAM_ACCESS_NAME + structmember + "(" + functionargs + ", " + str(arrayrefoffset) + ")"
+									print "\tReplacement: " + call
+									replacementnode = c_ast.ID("name")
+									setattr(replacementnode, 'name', call)
+									replace_node(node.parent, replacementnode)
+								else:
+									print "Unsupported ArrayRef format detected"
+							else:
+								call = "juniper_ram_fetch_" + structmember + "(" + functionargs + ", 0)"
+								print "\tReplacement: " + call
+								replacementnode = c_ast.ID("name")
+								setattr(replacementnode, 'name', call)
+								replace_node(node, replacementnode)
+						
+	v = Visitor()
+	v.visit(ast)
--- utils.py
+++ utils.py
@@ -8,102 +8,102 @@ import utils
 
 
 def getlineoffile(filename, lineno):
-    """
-    Return a string of line number lineno of file filename. This is a slow operation
-    and should be called sparingly.
-    """
-    try:
-        fp = open(filename)
-        for i, line in enumerate(fp):
-            if i == (lineno - 1):
-                return line
-    finally:
-        fp.close()
+	"""
+	Return a string of line number lineno of file filename. This is a slow operation
+	and should be called sparingly.
+	"""
+	try:
+		fp = open(filename)
+		for i, line in enumerate(fp):
+			if i == (lineno - 1):
+				return line
+	finally:
+		fp.close()
 
 
 def deglob_file(globbedname):
-    """
-    Jamaicabuilder outputs C files of the form PKG_<Java package name>_V<Some hex>.c
-    As we don't know what the hex is, we can glob it and warn if more than one file matches.
-    """
-    matches = glob.glob(str(globbedname))
-    if len(matches) == 0:
-        print("The filename \"" + str(globbedname) + "\" does not match any files.")
-        sys.exit(-1)
-    if len(matches) > 1:
-        print("Warning: Multiple matches for filename " + str(globbedname) + "\"")
-        print("Using: " + matches[0])
-        return matches[0]
-    return matches[0]
+	"""
+	Jamaicabuilder outputs C files of the form PKG_<Java package name>_V<Some hex>.c
+	As we don't know what the hex is, we can glob it and warn if more than one file matches.
+	"""
+	matches = glob.glob(str(globbedname))
+	if len(matches) == 0:
+		print("The filename \"" + str(globbedname) + "\" does not match any files.")
+		sys.exit(-1)
+	if len(matches) > 1:
+		print("Warning: Multiple matches for filename " + str(globbedname) + "\"")
+		print("Using: " + matches[0])
+		return matches[0]
+	return matches[0]
 
 
 def get_original_name_of_fn(ast, funcname):
-    """
-    Search for a function definition of name funcname. If found, extract the original 
-    Java version of the function (which will be inserted as a block comment by Jamaica)
-    """
-    class FuncDefVisitor(c_ast.NodeVisitor):
-        def __init__(self):
-            self.found = None
-        
-        def visit_FuncDef(self, node):
-            pattern = re.compile(
-                r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
-                re.DOTALL | re.MULTILINE
-            )        
-            line = utils.getlineoffile(node.decl.coord.file, node.decl.coord.line)
-            comments = re.findall(pattern, line)
-            if len(comments) == 1:
-                c = comments[0]
-                if c[:2] == "/*" and c[-2:] == "*/":
-                    origname = c[2:-2].strip()
-                    if funcname == None:
-                        print "Function " + node.decl.name    + " originally: " + origname
-                    else:
-                        if node.decl.name == funcname:
-                            self.found = origname
-                
-    v = FuncDefVisitor()
-    v.visit(ast)
-    return v.found
+	"""
+	Search for a function definition of name funcname. If found, extract the original 
+	Java version of the function (which will be inserted as a block comment by Jamaica)
+	"""
+	class FuncDefVisitor(c_ast.NodeVisitor):
+		def __init__(self):
+			self.found = None
+		
+		def visit_FuncDef(self, node):
+			pattern = re.compile(
+				r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+				re.DOTALL | re.MULTILINE
+			)        
+			line = utils.getlineoffile(node.decl.coord.file, node.decl.coord.line)
+			comments = re.findall(pattern, line)
+			if len(comments) == 1:
+				c = comments[0]
+				if c[:2] == "/*" and c[-2:] == "*/":
+					origname = c[2:-2].strip()
+					if funcname == None:
+						print "Function " + node.decl.name    + " originally: " + origname
+					else:
+						if node.decl.name == funcname:
+							self.found = origname
+				
+	v = FuncDefVisitor()
+	v.visit(ast)
+	return v.found
 
 
 def parse_jamaica_output(filename):
-    """
-    Use pycparser to parse a Jamaica output file.
-    Because Jamaica's output is pretty complex, cpp is required, and even still 
-    some features have to be removed because it uses GCC extensions unsupported by pycparser.
-    Returns a pycparser.c_ast or throws ParseError
-    """
-    if '*' in filename:
-        filename = deglob_file(filename)
-    
-    cppargs = ['-E', 
-               '-DNDEBUG', #Disable Jamaica debug support 
-               '-U__GNUC__', #Prevents use of __attribute__, will cause an "unsupported compiler" #warning
-               '-W#warnings', #And this hides that warning
-               '-DJAMAICA_NATIVE_TIME_GET_HIGH_RESOLUTION_TIMESTAMP', #These are inline asm which seems not supported by pycparser
-               '-DJAMAICA_NATIVE_THREAD_COMPARE_AND_SWAP',
-               r'-I/Users/ian/Documents/pycparser/utils/fake_libc_include', #Override standard libc with pycparser's minimal version
-               #r'-I/Users/ian/Documents/jamaica/current/target/linux-x86_64/include'
-               r'-I/Users/ian/Dropbox/Work/VivadoHLS/jamaica/include'
-               ]    
-    
-    return pycparser.parse_file(filename, use_cpp=True, cpp_path="gcc", cpp_args=cppargs)
+	"""
+	Use pycparser to parse a Jamaica output file.
+	Because Jamaica's output is pretty complex, cpp is required, and even still 
+	some features have to be removed because it uses GCC extensions unsupported by pycparser.
+	Returns a pycparser.c_ast or throws ParseError
+	"""
+	if '*' in filename:
+		filename = deglob_file(filename)
+	
+	cppargs = ['-E', 
+			   '-DNDEBUG', #Disable Jamaica debug support 
+			   '-U__GNUC__', #Prevents use of __attribute__, will cause an "unsupported compiler" #warning
+			   '-W#warnings', #And this hides that warning
+			   '-DJAMAICA_NATIVE_TIME_GET_HIGH_RESOLUTION_TIMESTAMP', #These are inline asm which seems not supported by pycparser
+			   '-DJAMAICA_NATIVE_THREAD_COMPARE_AND_SWAP',
+			   r'-I/Users/ian/Documents/pycparser/utils/fake_libc_include', #Override standard libc with pycparser's minimal version
+			   #r'-I/Users/ian/Documents/jamaica/current/target/linux-x86_64/include'
+			   r'-I/Users/ian/Dropbox/Work/VivadoHLS/jamaica/include'
+			   ]    
+	
+	return pycparser.parse_file(filename, use_cpp=True, cpp_path="gcc", cpp_args=cppargs)
 
 
 def write_ast_to_file(ast, filename):
-    """
-    Run the provided AST through pycparser's C generator and write to filename. Will
-    overwrite if filename exists. Will fail if filename points into a directory which 
-    does not exist.
-    """
-    generator = pycparser.c_generator.CGenerator()
-    code = generator.visit(ast)
-    outf = open(filename, "w+")
-    try:
-        outf.writelines(code)
-    finally:
-        outf.close()
-    
-    
+	"""
+	Run the provided AST through pycparser's C generator and write to filename. Will
+	overwrite if filename exists. Will fail if filename points into a directory which 
+	does not exist.
+	"""
+	generator = pycparser.c_generator.CGenerator()
+	code = generator.visit(ast)
+	outf = open(filename, "w+")
+	try:
+		outf.writelines(code)
+	finally:
+		outf.close()
+	
+	
