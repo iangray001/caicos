aa27c4708724c6b651bc37305a09cf7be996175b
Ian Gray <iangray001@github.com>
2015-03-11 11:38:48 +0000

    Changed RAM access functions to get and set

--- projectfiles/include/fpgaporting.h
+++ projectfiles/include/fpgaporting.h
@@ -10,14 +10,23 @@
 void create_jamaica_thread(jamaica_thread *t);
 
 //RAM Access functions
-jamaica_ref juniper_ram_fetch_r(int addr, int subwordoffset);
-jamaica_int32 juniper_ram_fetch_i(int addr, int subwordoffset);
-jamaica_uint32 juniper_ram_fetch_ui(int addr, int subwordoffset);
-jamaica_int16 juniper_ram_fetch_s(int addr, int subwordoffset);
-jamaica_uint16 juniper_ram_fetch_us(int addr, int subwordoffset);
-jamaica_int8 juniper_ram_fetch_b(int addr, int subwordoffset);
-jamaica_uint8 juniper_ram_fetch_ub(int addr, int subwordoffset);
-jamaica_float juniper_ram_fetch_f(int addr, int subwordoffset);
+jamaica_ref juniper_ram_get_r(int addr, int subwordoffset);
+jamaica_int32 juniper_ram_get_i(int addr, int subwordoffset);
+jamaica_uint32 juniper_ram_get_ui(int addr, int subwordoffset);
+jamaica_int16 juniper_ram_get_s(int addr, int subwordoffset);
+jamaica_uint16 juniper_ram_get_us(int addr, int subwordoffset);
+jamaica_int8 juniper_ram_get_b(int addr, int subwordoffset);
+jamaica_uint8 juniper_ram_get_ub(int addr, int subwordoffset);
+jamaica_float juniper_ram_get_f(int addr, int subwordoffset);
+
+void juniper_ram_set_r(int addr, int subwordoffset, jamaica_ref r);
+void juniper_ram_set_i(int addr, int subwordoffset, jamaica_int32 i);
+void juniper_ram_set_ui(int addr, int subwordoffset, jamaica_uint32 ui);
+void juniper_ram_set_s(int addr, int subwordoffset, jamaica_int16 s);
+void juniper_ram_set_us(int addr, int subwordoffset, jamaica_uint16 us);
+void juniper_ram_set_b(int addr, int subwordoffset, jamaica_int8 b);
+void juniper_ram_set_ub(int addr, int subwordoffset, jamaica_uint8 ub);
+void juniper_ram_set_f(int addr, int subwordoffset, jamaica_float f);
 
 extern volatile jamaica_data32 *__juniper_ram_master;
 
