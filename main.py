2891ba4c054066885679615844a5c48682b19fce
Ian Gray <iangray001@github.com>
2015-04-19 13:06:45 +0100

    Support for the dev_softwareonly config option

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ and to read and parse configuration files.
 Call build_all() to begin.
 """
 
-import os
-import shutil
-
-import prepare_hls_project
-import prepare_src_project
-from utils import mkdir, CaicosError
-
-
 """
 Describes the required options in the configuration of caicos.
 All valid options must be enumerated in this dictionary. Each option name maps
@@ -24,28 +16,30 @@ added as a member of an array with that name.
 
 Multiple definitions of the same variable are valid, and the last definition
 takes precedence.
-
-Summary of the config options:
-	Required:
-		signatures - list of Java method signatures to be compiled to hardware
-		jamaicabuilderoutputdir - path to compiled C files from jamaicabuilder
-		outputdir - path to directory to output to
-		fpgapart - the FPGA part to target, in Xilinx format
-		jamaicatarget - path to the Jamaica target directory to use for the host software  
-	
-	Optional:
-		additionalhardwarefiles - list of paths to additional files to add to the hardware project
-		cleanoutput - if 'true', the contents of outputdir will be cleaned first  
 """
+
+import os
+import shutil
+
+import prepare_hls_project
+import prepare_src_project
+from utils import mkdir, CaicosError, log
+
+
 config_specification = {
-	'signatures': (True, ""),
-	'jamaicabuilderoutputdir': (True, ""),
-	'additionalhardwarefiles': (False, ""),
-	'outputdir': (True, ""),
-	'fpgapart': (True, ""),
-	'jamaicatarget': (True, ""),
-	'cleanoutput': (False, ""),
+	#Standard options
+	'signatures': (True, ""), # list of Java method signatures to be compiled to hardware
+	'jamaicabuilderoutputdir': (True, ""), # path to compiled C files from jamaicabuilder
+	'additionalhardwarefiles': (False, ""), # list of paths to additional files to add to the hardware project
+	'outputdir': (True, ""), # path to directory to output to
+	'fpgapart': (True, ""), # the FPGA part to target, in Xilinx format
+	'jamaicatarget': (True, ""), # path to the Jamaica target directory to use for the host software  
+	'cleanoutput': (False, ""), # if 'true', the contents of outputdir will be cleaned first
 	
+	#Developer options
+	'dev_softwareonly': (False, ""),
+	
+	#Array singletons
 	'signature': (False, "signatures"),
 	'additionalhardwarefile': (False, "additionalhardwarefiles"),
 }
@@ -63,16 +57,19 @@ def build_all(config):
 	cwd = os.path.dirname(os.path.realpath(__file__))
 	mkdir(config['outputdir'])
 	
-	bindings = prepare_hls_project.build_from_functions(
-		config['signatures'], 
-		config['jamaicabuilderoutputdir'],
-		os.path.join(config['outputdir'], "hardware"), 
-		config.get('additionalhardwarefiles'), 
-		config['fpgapart'])
-
-	shutil.copyfile(os.path.join(cwd, "projectfiles", "scripts", "push.sh"), os.path.join(config['outputdir'], "hardware", "push.sh"))
+	if config.get('dev_softwareonly', "false").lower() == "true":
+		log().warning("dev_softwareonly is set. Generating software only.")
+		bindings = __getfakebindings(config['signatures'])
+	else:		
+		bindings = prepare_hls_project.build_from_functions(
+			config['signatures'], 
+			config['jamaicabuilderoutputdir'],
+			os.path.join(config['outputdir'], "hardware"), 
+			config.get('additionalhardwarefiles'), 
+			config['fpgapart'])
 	
-	
+		shutil.copyfile(os.path.join(cwd, "projectfiles", "scripts", "push.sh"), os.path.join(config['outputdir'], "hardware", "push.sh"))
+		
 	prepare_src_project.build_src_project(
 		bindings,
 		config['jamaicabuilderoutputdir'],
@@ -130,4 +127,19 @@ def load_config(filename):
 				raise CaicosError("Required parameter '" + str(p) + "' was not defined by config file " + str(filename))
 		
 	return config
+
+
+
+
+def __getfakebindings(sigs):
+	"""
+	Generate a fake bindings dictionary. Only used when in developer mode with dev_softwareonly
+	"""
+	b = {}
+	callid = 0
+	for s in sigs:
+		b[callid] = s
+		callid = callid + 1
+	return b
+
 
