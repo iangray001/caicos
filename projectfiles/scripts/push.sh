723eb2023516243ffcde694d8cd3351837e6ef7c
Ian Gray <iangray001@github.com>
2015-04-28 20:45:57 +0100

    push.sh can now be executed from any working directory

--- projectfiles/scripts/push.sh
+++ projectfiles/scripts/push.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-PROJECT=${PWD##*/}
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PROJECT=caicosout
 HOSTNAME=pegasus
 HOSTNAMETEMP=/home/iang/vivado
 
 echo -e "\033[31m===========================================================================\033[0m"
 echo -e "\033[31m===========================================================================\033[0m"
-
-#Delete and remake the target directory
 
 if [ -z "$HOSTNAMETEMP" ]; then
 	echo "Temp directory is unset."
@@ -19,6 +18,7 @@ else
 	fi
 fi
 
+#Delete and remake the target directory
 if [ "clean" = "$1" ]; then
 	echo "Cleaning remote directory $HOSTNAMETEMP/$PROJECT..."
 	ssh -q $HOSTNAME "rm -rf $HOSTNAMETEMP/$PROJECT"
@@ -27,7 +27,7 @@ fi
 ssh -q $HOSTNAME "mkdir $HOSTNAMETEMP/$PROJECT"
 
 echo -n "Copying project $PROJECT..."
-scp -q -r * $HOSTNAME:$HOSTNAMETEMP/$PROJECT/
+scp -q -r $DIR/* $HOSTNAME:$HOSTNAMETEMP/$PROJECT/
 echo "done."
 
 #Build the thing
