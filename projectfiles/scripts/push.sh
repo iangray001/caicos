6cb265177636e16d1b989390b5865a82c63ab13a
Ian Gray <iangray001@github.com>
2015-08-10 11:53:30 +0100

    Now only invokes the HLS build when instructed (with 'build') as this is normally handled by the other hardware scripts

--- projectfiles/scripts/push.sh
+++ projectfiles/scripts/push.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-PROJECT=caicosout
 HOSTNAME=pegasus
 HOSTNAMETEMP=/home/iang/vc707_autogen_pr/reconfig
 
-echo -e "\033[31m===========================================================================\033[0m"
-echo -e "\033[31m===========================================================================\033[0m"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PROJECT=caicosout
 
-if [ -z "$HOSTNAMETEMP" ]; then
-	echo "Temp directory is unset."
-	exit
-else
-	if [ -z "$PROJECT" ]; then
-		echo "Project name is unset"
-		exit
-	fi
-fi
+echo -e "\033[31m===========================================================================\033[0m"
 
 #Delete and remake the target directory
-if [ "clean" = "$1" ]; then
-	echo "Cleaning remote directory $HOSTNAMETEMP/$PROJECT..."
-	ssh -q $HOSTNAME "rm -rf $HOSTNAMETEMP/$PROJECT"
-fi
-
+echo "Cleaning remote directory $HOSTNAMETEMP/$PROJECT..."
+ssh -q $HOSTNAME "rm -rf $HOSTNAMETEMP/$PROJECT"
 ssh -q $HOSTNAME "mkdir $HOSTNAMETEMP/$PROJECT"
 
 echo -n "Copying project $PROJECT..."
 scp -q -r $DIR/* $HOSTNAME:$HOSTNAMETEMP/$PROJECT/
 echo "done."
 
-#Build the thing
-ssh -q $HOSTNAME "cd $HOSTNAMETEMP/$PROJECT/; . ~/xilinx14.3.sh; vivado_hls script.tcl"
-
-#Bring back the report
-scp -q $HOSTNAME:$HOSTNAMETEMP/$PROJECT/prj/solution1/syn/report/hls_csynth.rpt .
+#Maybe build
+if [ "build" = "$1" ]; then
+	ssh -q $HOSTNAME "cd $HOSTNAMETEMP/$PROJECT/; . ~/xilinx14.3.sh; vivado_hls script.tcl"
+	
+	#Bring back the report
+	scp -q $HOSTNAME:$HOSTNAMETEMP/$PROJECT/prj/solution1/syn/report/hls_csynth.rpt .
+fi
