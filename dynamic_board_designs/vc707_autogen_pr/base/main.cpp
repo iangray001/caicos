16d0b849b702ebddbd2ed74aa23aa08dafc071c7
Ian Gray <iangray001@github.com>
2015-08-11 14:16:33 +0100

    Revert base to CPP

--- dynamic_board_designs/vc707_autogen_pr/base/main.cpp
+++ dynamic_board_designs/vc707_autogen_pr/base/main.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
-#include <ap_cint.h>
+#include <ap_int.h>
 
 typedef struct
 {
@@ -22,7 +22,7 @@ volatile short *__juniper_ram_master_short;
 volatile float *__juniper_ram_master_float;
 #endif
 
-volatile uint1 syscall_interrupt;
+volatile ap_uint<1>   jamaica_syscall;
 volatile PCIE_Syscall syscall_args;
 
 void fix_hls_compiler()
@@ -52,11 +52,11 @@ int hls(int* opid, int* arg1, int* arg2, int* arg3)
 #pragma HLS INTERFACE s_axilite port=syscall_args bundle=AXILiteS register
 #pragma HLS INTERFACE s_axilite port=return bundle=AXILiteS register
 
-#pragma HLS INTERFACE ap_none port=syscall_interrupt register
+#pragma HLS INTERFACE ap_none port=jamaica_syscall register
 
     // Annoyingly, it has to do *something*
     //fix_hls_compiler();
-	syscall_interrupt = 0;
+    jamaica_syscall = 0;
 
     syscall_args.cmd = 0;
     syscall_args.arg1 = 0;
