a082140a5af3e5157c3930241fb7826a5641741e
Ian Gray <iangray001@github.com>
2015-08-11 14:05:52 +0100

    Revert breaking change

--- dynamic_board_designs/vc707_autogen_pr/base/autobuild.tcl
+++ dynamic_board_designs/vc707_autogen_pr/base/autobuild.tcl
@@ -1,18 +1,25 @@
-open_project "prj"
+open_project "juniperHLS"
 open_solution "solution1"
+
 set_top hls
 
-foreach f [exec ls | grep -F .c] {
-    add_files $f -cflags "-I."
+foreach f [exec ls | grep -F .cpp] {
+    add_files $f -cflags "-I../../projectfiles/include/"
 }
 
 foreach f [exec ls | grep -F .h] {
-    add_files $f
+    add_files $f -cflags "-I../../projectfiles/include/"
 }
 
 # Use VC707
 set_part {xc7vx485tffg1761-2}
 
 create_clock -period 10 -name default
+
+# Create some hardware
 csynth_design
+
+# And dump to ISE
 export_design -format syn_dcp -description "caicos dynamic hardware project" -vendor "york.ac.uk" -version "1.0"
+
+exit
