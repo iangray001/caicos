98f12bde929d0f019c0a5e883c34a62241e8207d
Ian Gray <iangray001@github.com>
2015-09-17 14:21:06 +0100

    Inserts check for root

--- projectfiles/scripts/program.sh
+++ projectfiles/scripts/program.sh
@@ -1,5 +1,10 @@
 # Given a bitfile as argument 1, prepare and execute an impact batch file to program the FPGA.
 
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
 if [ -z "$1" ]; then
 	echo "Usage: $0 <bitfile>"
 	exit 1
