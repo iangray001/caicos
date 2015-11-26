69b38becfb8e62f23679e189ea9a541c4e52ef19
Jamie Garside <jamie.garside@york.ac.uk>
2015-11-26 00:15:37 +0000

    Fixed make_reconfig.tcl - in Tcl, argv doesn't contain the app name, hence the first argument is argv[0], not argv[1].

--- dynamic_board_designs/common/make_reconfig.tcl
+++ dynamic_board_designs/common/make_reconfig.tcl
@@ -4,6 +4,6 @@ if { $argc != 1 } {
 	error "Checkpoint name not supplied, aborting"
 }
 
-set reconfig_checkpoint [lindex $argv 1]
+set reconfig_checkpoint [lindex $argv 0]
 
 build_reconfig_design $project_config $reconfig_checkpoint
